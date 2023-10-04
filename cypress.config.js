552422638b7f3eb74e24c95d4245cd4da41ae54f
ProvatSutradhar <s.provatcsd01@gmail.com>
2023-10-04 18:45:49 +0600

    changed test browser resolution to 1376x720 from default

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    viewportWidth: 1376,
+    viewportHeight: 720,
     baseUrl: 'https://www.tvhut.com.bd/',
   },
 })
